620f9a0a0a635dabb1789a9f9eb2a4cdb12b127f
Emma Lim <116303045+emmalim@users.noreply.github.com>
2022-12-22 19:39:59 -0500

    work page display (mobile)

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -4,12 +4,12 @@ js file for Emma's portfolio
 
 /*clicking on the button will toggle btwn hiding/showing the  nav menu*/
 function toggleNav() {
-  var menu = document.getElementsByClassName("dropdownMenu");
-  if (menu.style.display === "none") {
-    menu.style.display = "block";
-  } else {
-    menu.style.display = "none";
-  }
+    var menu = document.getElementsByClassName("dropdownMenu");
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
 }
 
 
